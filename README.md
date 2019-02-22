0932cd0d034fb466d42e1ea7713ca77696072d5f
Andrew Kelley <andrew@ziglang.org>
2019-02-21 20:11:19 -0500

    here's the code from the stream 2019-02-21

---
+++ README.md
@@ -0,0 +1,114 @@
+# GeneralPurposeDebugAllocator
+
+This is the code for [my Zig Live Coding Stream](https://www.twitch.tv/andrewrok).
+[Support my work on Patreon](https://www.patreon.com/andrewrk).
+
+This is a work-in-progress general purpose allocator intended to be eventually merged
+into the [Zig](https://ziglang.org/) standard library, with the focus on these goals:
+
+ * Detect double free, and print stack trace of:
+   - Where it was first allocated
+   - Where it was freed the first time
+   - Where it was freed the second time
+
+ * Detect leaks and print stack trace of:
+   - Where it was allocated
+
+ * Ideally, freed memory would be unmapped so that it
+   would cause page faults if used.
+
+ * Make pointer math errors unlikely to harm memory from
+   unrelated allocations
+
+ * It's OK for these mechanisms to cost some extra bytes and for
+   memory to become a little fragmented.
+
+ * OK for performance cost for these mechanisms.
+
+## Goals for Other General Purpose Allocators But Not This One
+
+ReleaseFast and ReleaseSmall Modes:
+
+ * Low fragmentation is primary concern
+ * Performance of worst-case latency is secondary concern
+ * Performance of average-case latency is next
+ * Finally, having freed memory unmapped, and pointer math errors unlikely to
+   harm memory from unrelated allocations are nice-to-haves.
+
+ReleaseSafe Mode:
+
+ * Low fragmentation is primary concern
+ * All the safety mechanisms from Debug Mode are the next concern.
+ * It's OK for these mechanisms to take up some percent overhead
+   of memory, but not at the cost of fragmentation, which can cause
+   the equivalent of memory leaks.
+
+## Current Status
+
+POSIX-only so far.
+
+Able to detect memory leaks:
+
+```
+zig test test.zig
+Test 1/1 oaeu...alloc1 = i32@7f27fbfda000
+alloc2 = i32@7f27fbfda004
+alloc1 = i32@7f27fbfda008
+alloc2 = i32@7f27fbfda00c
+alloc1 = i32@7f27fbfda010
+alloc2 = i32@7f27fbfda014
+alloc1 = i32@7f27fbfda018
+alloc2 = i32@7f27fbfda01c
+
+Memory leak detected:
+
+Memory leak detected:
+
+Memory leak detected:
+
+Memory leak detected:
+OK
+All tests passed.
+```
+
+Only able to allocate 1 memory page per bucket, and does not support
+large allocations.
+
+### Current Design
+
+Small allocations are divided into buckets:
+
+```
+index obj_size
+0     1
+1     2
+2     4
+3     8
+4     16
+5     32
+6     64
+7     128
+```
+
+Each bucket starts with no pages allocated. When the first object is allocated
+for a given bucket, it allocates 1 page of memory from the OS. Each successive
+allocation grabs the next slot from the bucket. Once all slots are exhausted
+the allocator always returns OutOfMemory.
+
+Each bucket has 1 bit per slot representing whether the slot is used. Allocations
+assert that the corresponding bit is 0 and set it to 1. Frees assert that the
+corresponding bit is 1 and set it to 0.
+
+## Roadmap
+
+* Make the leak detector print stack traces of the allocations
+* Make it reuse freed memory
+* Make it support allocations after one page is exhausted
+* Make it support allocations larger than what fits in the small allocation buckets
+* Prevent more kinds of errors
+* Give memory back to the OS as often as possible. If a page can be unmapped then it
+  should be unmapped.
+* Clear read/write permission bits on the allocator state itself, outside of function
+  calls, so that non-allocator code cannot modify its state.
+* Validation fuzz testing
+* Performance benchmarking
---
+++ test.zig
@@ -0,0 +1,162 @@
+const std = @import("std");
+const assert = std.debug.assert;
+const Allocator = std.mem.Allocator;
+const page_size = std.os.page_size;
+const posix = std.os.posix;
+
+fn up_to_nearest_power_of_2(comptime T: type, n: T) T {
+    var power: T = 1;
+    while (power < n)
+        power *= 2;
+    return power;
+}
+
+const GeneralPurposeDebugAllocator = struct {
+    allocator: Allocator,
+    buckets: [small_bucket_count]Bucket,
+    used_bytes: [used_bytes_len]u8,
+
+    const used_bytes_len = blk: {
+        var total = 0;
+        var i = 0;
+        while (i < small_bucket_count) : (i += 1) {
+            const obj_size = 1 << i;
+            const this_bucket_used_bytes = page_size / (8 * obj_size);
+            total += this_bucket_used_bytes;
+        }
+        break :blk total;
+    };
+
+    const small_bucket_count = 8;
+
+    const Bucket = struct {
+        ptr: ?[*]u8,
+        used_bits: []u8,
+        end_index: usize,
+    };
+
+    pub fn init(self: *GeneralPurposeDebugAllocator) void {
+        self.* = GeneralPurposeDebugAllocator{
+            .allocator = Allocator{
+                .allocFn = alloc,
+                .reallocFn = realloc,
+                .freeFn = free,
+            },
+            .buckets = undefined,
+            .used_bytes = [1]u8{0} ** used_bytes_len,
+        };
+        var offset: usize = 0;
+        for (self.buckets) |*bucket, i| {
+            const obj_size = usize(1) << @intCast(u6, i);
+            const this_bucket_used_bytes = page_size / (8 * obj_size);
+            const next_offset = offset + this_bucket_used_bytes;
+            bucket.* = Bucket{
+                .ptr = null,
+                .end_index = 0,
+                .used_bits = self.used_bytes[offset..next_offset],
+            };
+            offset = next_offset;
+        }
+    }
+
+    pub fn detectLeaks(self: *GeneralPurposeDebugAllocator) void {
+        for (self.buckets) |*bucket| {
+            for (bucket.used_bits) |used_byte| {
+                if (used_byte != 0) {
+                    var bit_index: u3 = 0;
+                    while (true) : (bit_index += 1) {
+                        const is_used = @truncate(u1, used_byte >> bit_index) != 0;
+                        if (is_used) {
+                            std.debug.warn("\nMemory leak detected:\n");
+                            // TODO
+                        }
+                        if (bit_index == std.math.maxInt(u3))
+                            break;
+                    }
+                }
+            }
+        }
+    }
+
+    fn alloc(allocator: *Allocator, n: usize, alignment: u29) error{OutOfMemory}![]u8 {
+        const self = @fieldParentPtr(GeneralPurposeDebugAllocator, "allocator", allocator);
+        if (n > (1 << (small_bucket_count - 1))) {
+            return error.OutOfMemory;
+        }
+        // round n up to nearest power of 2
+        const rounded_n = up_to_nearest_power_of_2(usize, n);
+        const bucket_index = std.math.log2(rounded_n);
+        const bucket = &self.buckets[bucket_index];
+        if (bucket.end_index == page_size)
+            return error.OutOfMemory;
+        const ptr = bucket.ptr orelse blk: {
+            const perms = posix.PROT_READ | posix.PROT_WRITE;
+            const flags = posix.MAP_PRIVATE | posix.MAP_ANONYMOUS;
+            const addr = posix.mmap(null, page_size, perms, flags, -1, 0);
+            if (addr == posix.MAP_FAILED) return error.OutOfMemory;
+            const ptr = @intToPtr([*]align(page_size) u8, addr);
+            bucket.ptr = ptr;
+            break :blk ptr;
+        };
+        // what byte and bit does the slot correspond to?
+        const slot_index = bucket.end_index / rounded_n;
+        const used_byte_index = slot_index / 8;
+        const used_bit_index = @intCast(u3, slot_index % 8);
+        const used_byte = bucket.used_bits[used_byte_index];
+        const is_used = @truncate(u1, used_byte >> used_bit_index) != 0;
+        assert(!is_used);
+        bucket.used_bits[used_byte_index] = used_byte | (u8(1) << used_bit_index);
+
+        const result = (ptr + bucket.end_index)[0..n];
+        bucket.end_index += rounded_n;
+        assert(result.len != 0);
+        return result;
+    }
+
+    fn realloc(
+        allocator: *Allocator,
+        old_mem: []u8,
+        new_size: usize,
+        alignment: u29,
+    ) error{OutOfMemory}![]u8 {
+        //const self = @fieldParentPtr(GeneralPurposeDebugAllocator, "allocator", allocator);
+        if (new_size <= old_mem.len) {
+            return old_mem[0..new_size];
+        }
+        const new_mem = try alloc(allocator, new_size, alignment);
+        @memcpy(new_mem.ptr, old_mem.ptr, old_mem.len);
+        return new_mem;
+    }
+
+    fn free(allocator: *Allocator, bytes: []u8) void {
+        const self = @fieldParentPtr(GeneralPurposeDebugAllocator, "allocator", allocator);
+        const rounded_n = up_to_nearest_power_of_2(usize, bytes.len);
+        const bucket_index = std.math.log2(rounded_n);
+        const bucket = &self.buckets[bucket_index];
+        const bucket_ptr = bucket.ptr.?;
+        const byte_offset = @ptrToInt(bytes.ptr) - @ptrToInt(bucket.ptr);
+        const slot_index = byte_offset / rounded_n;
+        const used_byte_index = slot_index / 8;
+        const used_bit_index = @intCast(u3, slot_index % 8);
+        const used_byte = bucket.used_bits[used_byte_index];
+        const is_used = @truncate(u1, used_byte >> used_bit_index) != 0;
+        assert(is_used);
+        bucket.used_bits[used_byte_index] = used_byte & ~(u8(1) << used_bit_index);
+    }
+};
+
+test "oaeu" {
+    var gpda: GeneralPurposeDebugAllocator = undefined;
+    gpda.init();
+    defer gpda.detectLeaks();
+    const allocator = &gpda.allocator;
+
+    var i: usize = 0;
+    while (i < 4) : (i += 1) {
+        const alloc1 = try allocator.create(i32);
+        std.debug.warn("alloc1 = {}\n", alloc1);
+        const alloc2 = try allocator.create(i32);
+        std.debug.warn("alloc2 = {}\n", alloc2);
+        allocator.destroy(alloc1);
+    }
+}
